e6b121cff189ebae1bcc13c8b9f1fb019d1a8172
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-04-18 16:41:58 +0200

    ui-component

---
+++ ui-component.js
@@ -0,0 +1,19 @@
+function simple_elevator_handler() {
+	// read user input from dom
+
+	var result = simple_elevator(floor, button);
+
+	// write user input to dom
+}
+
+function render_simple_elevator(container) {
+	// grab container from dom
+
+	// build all necessary dom elements
+
+	// append them how they should be
+
+	// attach event listeners
+}
+
+render_simple_elevator("simple-elevator");
